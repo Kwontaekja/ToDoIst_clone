01d583d7b26fdab6fd6b2002145ebf5d8ddedbe8
Kwontaekja <taek.ja.twentytwo@gmail.com>
2020-07-08 14:08:00 +0900

    clock_added_js

--- main.js
+++ main.js
@@ -16,24 +16,3 @@ function titleClickHandler() {
 }
 
 title.addEventListener("click", titleClickHandler);
-
-const clock = document.querySelector(".clock");
-
-function getTime() {
-  const date = new Date();
-  const Hours = date.getHours(),
-    Minutes = date.getMinutes(),
-    Seconds = date.getSeconds();
-  clock.innerHTML = `${Hours >= 10 ? `${Hours}` : `0${Hours}`}:${
-    Minutes >= 10 ? `${Minutes}` : `0${Minutes}`
-  }:${Seconds >= 10 ? `${Seconds}` : `0${Seconds}`}`;
-}
-
-function init() {
-  getTime();
-}
-
-init();
-
-// milliseconds 기준
-setInterval(getTime, 1000);
