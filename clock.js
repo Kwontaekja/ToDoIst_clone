78d0e788fe9bad31ec425076b894fc6d6e5542f5
Kwontaekja <taek.ja.twentytwo@gmail.com>
2020-07-08 18:05:43 +0900

    clock_upgrade

--- clock.js
+++ clock.js
@@ -1,14 +1,49 @@
-const clock = document.querySelector(".clock");
+const clock = document.querySelector(".clock_ex");
 
+const months = [
+  "JAN",
+  "FEB",
+  "MAR",
+  "APR",
+  "MAY",
+  "JUN",
+  "JUL",
+  "AUG",
+  "SEP",
+  "OCT",
+  "NOV",
+  "DEC",
+];
+
+var AMPM = "AM";
+
+const days = ["SUN", "MON", "WED", "TUE", "THU", "FRI", "SAT"];
 function getTime() {
   const date = new Date();
-  const Hours = date.getHours(),
+  var Hours = date.getHours(),
     Minutes = date.getMinutes(),
-    Seconds = date.getSeconds();
-  clock.innerHTML = `${Hours >= 10 ? `${Hours}` : `0${Hours}`}:${
+    Seconds = date.getSeconds(),
+    Month = months[date.getMonth()],
+    _date = date.getDate(),
+    Day = days[date.getDay()];
+
+  if (Hours >= 12) {
+    if (Minutes > 0 && Seconds > 0) {
+      AMPM = "PM";
+      Hours -= 12;
+    } else {
+      AMPM = "AM";
+    }
+  } else {
+    AMPM = "AM";
+  }
+
+  clock.innerHTML = `${Month} ${_date} (${Day}) - ${AMPM} ${Hours}:${
     Minutes >= 10 ? `${Minutes}` : `0${Minutes}`
   }:${Seconds >= 10 ? `${Seconds}` : `0${Seconds}`}`;
 }
+//   : ${ Seconds >= 10 ? `${Seconds}` : `0${Seconds}` }
+// Mon Date Day - AM/PM H:MM:SS
 
 function init() {
   getTime();
--- main.js
+++ main.js
@@ -6,13 +6,15 @@ function titleClickHandler() {
   const unclicked = !title.classList.contains(clcikedClassName);
 
   //   아래와 같이 toggle function으로 한번에 처리가 가능하다
-  //   if (unclicked) {
-  //     title.classList.add(clcikedClassName);
-  //   } else {
-  //     title.classList.remove(clcikedClassName);
-  //   }
+  if (unclicked) {
+    title.classList.add(clcikedClassName);
+    title.innerHTML = "Bye World!";
+  } else {
+    title.classList.remove(clcikedClassName);
+    title.innerHTML = "Hello World!";
+  }
 
-  title.classList.toggle(clcikedClassName);
+  //   title.classList.toggle(clcikedClassName);
 }
 
 title.addEventListener("click", titleClickHandler);
