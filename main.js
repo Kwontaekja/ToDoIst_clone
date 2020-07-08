3e0501bbb25c523211dd08dbcc74c1503610eae1
Kwontaekja <taek.ja.twentytwo@gmail.com>
2020-07-08 13:25:19 +0900

    background-modified

--- main.js
+++ main.js
@@ -5,11 +5,14 @@ const title = document.querySelector(".main_greeting");
 function titleClickHandler() {
   const unclicked = !title.classList.contains(clcikedClassName);
 
-  if (unclicked) {
-    title.classList.add(clcikedClassName);
-  } else {
-    title.classList.remove(clcikedClassName);
-  }
+  //   아래와 같이 toggle function으로 한번에 처리가 가능하다
+  //   if (unclicked) {
+  //     title.classList.add(clcikedClassName);
+  //   } else {
+  //     title.classList.remove(clcikedClassName);
+  //   }
+
+  title.classList.toggle(clcikedClassName);
 }
 
-title.addEventListener("clcik", titleClickHandler);
+title.addEventListener("click", titleClickHandler);
